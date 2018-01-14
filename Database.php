bc119347e0a4208a94088f8c90e6b98c208613f6
janjan <janjantandayag123@gmail.com>
2018-01-14 11:27:22 +0800

    Upload files

---
+++ Database.php
@@ -0,0 +1,74 @@
+<?php
+class Database {
+	private $connection = NULL;	
+	private $servername = 'localhost';
+	private $username = 'root';
+	private $password = '';
+	private $db_name = 'northwind';
+
+	public function __construct(){
+		try {
+			$this->connection = new PDO("mysql:host=$this->servername;dbname=$this->db_name", $this->username, $this->password);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $this->connection;
+		} catch(PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+		}
+	}
+
+	// SELECT
+	// public function selectData(){
+	// 	$stmt = $this->connection->prepare('SELECT first_name,last_name FROM customers');
+	// 	$stmt->execute();
+	// 	$stmt->setFetchMode(PDO::FETCH_ASSOC); 
+	// 	return $stmt->fetchAll();
+	// } 
+
+	// SELECT DISTINCT
+	// public function selectDistinct(){
+	// 	$stmt = $this->connection->prepare('SELECT COUNT(DISTINCT country_region) AS country_count FROM customers');
+	// 	$stmt->setFetchMode(PDO::FETCH_ASSOC); 
+	// 	$stmt->execute();
+	// 	return $stmt->fetchAll();
+	// } 
+
+	// WHERE
+	// public function where(){
+	// 	$stmt = $this->connection->prepare('SELECT * FROM customers WHERE customers.country_region = "PHILIPPINES"');
+	// 	$stmt->setFetchMode(PDO::FETCH_ASSOC); 
+	// 	$stmt->execute();
+	// 	return $stmt->fetchAll();
+	// }
+
+	// AND
+	// public function andOperator(){
+	// 	$stmt = $this->connection->prepare('SELECT * FROM customers,orders WHERE customers.id = 29 AND customers.id = orders.customer_id');
+	// 	$stmt->setFetchMode(PDO::FETCH_ASSOC); 
+	// 	$stmt->execute();
+	// 	return $stmt->fetchAll();
+	// }
+
+	// OR
+	// public function orOperator(){
+	// 	$stmt = $this->connection->prepare('SELECT * FROM customers WHERE customers.country_region="JAPAN" OR customers.country_region = "PHILIPPINES"');
+	// 	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	// 	$stmt->execute();
+	// 	return $stmt->fetchAll();
+	// }
+
+	// NOT 
+	// public function notOperator(){
+	// 	$stmt = $this->connection->prepare('SELECT * FROM customers WHERE customers.country_region NOT IN ("PHILIPPINES","USA")');
+	// 	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	// 	$stmt->execute();
+	// 	return $stmt->fetchAll(); 
+	// }
+
+	// MIXED OPERATORS
+	public function combineOperator(){
+		$stmt = $this->connection->prepare('SELECT * FROM customers WHERE customers.country_region NOT IN ("PHILIPPINES","USA")');
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		return $stmt->fetchAll(); 
+	}
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include('Database.php');
+$database = new Database();
+var_dump($database->notOperator());
+
